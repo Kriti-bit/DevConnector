950caad375f20ac0fbc33f85f03220bc29cbd250
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-15 13:37:08 +0530

    Routing landing, register and login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-        <Landing/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+          <Route exact path="/" component={ Landing } />
+          <div className="container">
+            <Route exact path = "/register" component = {Register}/>
+            <Route exact path = "/login" component = {Login}/>
+          </div>
+        <Footer/> 
+      </div>
+    </Router>
   );
 }
 
