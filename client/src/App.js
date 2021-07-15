e2e74604ad8a6aa3706706c2fb57f62620bfd2d1
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-15 11:44:44 +0530

    Footer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
         <h1>My React app</h1>
+      <Footer/>
     </div>
   );
 }
