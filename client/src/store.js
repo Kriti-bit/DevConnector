155f9b575a4a09707ee8c4128042b34fd52f0b7b
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-21 11:42:50 +0530

    Enabled redux extension

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,4 @@
-import { initial } from 'lodash';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
@@ -7,6 +6,11 @@ const initialState = {}
 
 const middleware = [thunk];
 
-const store = createStore( rootReducer, initialState, applyMiddleware(...middleware))
+const store = createStore( rootReducer, 
+    initialState, 
+    compose(applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    )
 
 export default store;
