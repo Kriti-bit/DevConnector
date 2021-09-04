359e170af98a47d7157d88e3db71e15ada866bf9
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-09-05 01:04:49 +0530

    added more textfields

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -4,9 +4,14 @@ import PropTypes from 'prop-types'
 
 const TextFieldGroup = ({ name, placeholder, value, label, error, info, type, onChange,disabled}) => {
     return (
-        <div className="form-group">
-            <input type={type} className={classnames('form-control form-control-lg', {'is-invalid':error})} placeholder={placeholder} name={name} value = {value} onChange = {onChange} disabled={disabled} />
-            {info && <small className="form-text text-muted">{info}</small>}
+
+        <div className="input-group mb-3">
+         <div className="input-group-prepend"></div>
+         <span className="input-group-text">
+         <i className={icon} />
+         </span>
+
+            <input className={classnames('form-control form-control-lg', {'is-invalid':error})} placeholder={placeholder} name={name} value = {value} onChange = {onChange} disabled={disabled} />
             {error && (<div className="invalid-feedback">{error}</div>)}
         </div>
     )
